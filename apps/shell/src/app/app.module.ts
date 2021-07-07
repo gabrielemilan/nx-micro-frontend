5ddcb4444b2cd4fbc9184e20cbccd5b163820230
Milan Gabriele <gabriele.milan@uefa.ch>
2021-07-07 13:55:56 +0200

    nxmfe

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
